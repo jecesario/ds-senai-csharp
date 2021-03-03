93c7aa9574433d98bc2bfbb4d6fcc1985ef98ebb
Jecesario <jp@soloid.com.br>
2021-03-03 11:41:19 -0300

    feat(ex1): resolvendo exercício 1

---
+++ Ex1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double imposto = 0.21;
+            double valorSemImposto;
+            valorSemImposto = 23.5;
+            double resultado;
+            resultado = imposto * valorSemImposto;
+            Console.WriteLine("Valor do imposto: " + resultado);
+            Console.ReadLine();
+        }
+    }
+}
