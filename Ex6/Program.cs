4efa5b99b17bd7c2d37569dd8e3845f1001f66d5
Jecesario <jp@soloid.com.br>
2021-03-03 12:02:20 -0300

    feat: resolvendo exercício 6 - porcentagem

---
+++ Ex6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            double a = double.Parse(Console.ReadLine());
+            a -= a * 0.085;
+            Console.WriteLine("Valor com desconto de 8,5%: " + a);
+
+            Console.Write("Digite outro número: ");
+            double b = double.Parse(Console.ReadLine());
+            b += b * 0.15;
+            Console.WriteLine("Valor com acrescimo de 15%: " + b);
+
+            Console.ReadLine();
+        }
+    }
+}
