fa97519276877308f1efc6ebb2aa00b21a3b5c58
Jecesario <jp@soloid.com.br>
2021-03-03 13:15:00 -0300

    feat: resolvendo exercicio 2 - maior numero

---
+++ Lista2/Ex2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Digite o primeiro numero: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo numero: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o terceiro numero: ");
+            int c = int.Parse(Console.ReadLine());
+
+            if(a > b && a > c) {
+                Console.WriteLine("O maior numero é: " + a);
+            } else if(b > c) {
+                Console.WriteLine("O maior numero é: " + b);
+            } else {
+                Console.WriteLine("O maior numero é: " + c);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
