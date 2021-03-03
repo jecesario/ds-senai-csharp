c9f88e3f113aace5016e42dbd225dcb892d6a419
Jecesario <jp@soloid.com.br>
2021-03-03 14:14:27 -0300

    feat: resolvendo exercício 4 - calculando ano bissexto

---
+++ Lista2/Ex4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Digite um ano: ");
+            int ano = int.Parse(Console.ReadLine());
+            Boolean bissexto = false;
+
+            if (ano % 4 == 0) {
+                if (ano % 100 != 0) {
+                    bissexto = true;
+                } else {
+                    if (ano % 400 == 0) {
+                        bissexto = true;
+                    }
+                }
+            }
+
+            if (bissexto == true) {
+                Console.WriteLine("O ano: " + ano + " é bissexto!");
+            } else {
+                Console.WriteLine("O ano: " + ano + " não é bissexto!");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
