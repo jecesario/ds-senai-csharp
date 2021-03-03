f5ac5b2cc69920d05650ce68f12983f5a657afb4
Jecesario <jp@soloid.com.br>
2021-03-03 13:30:02 -0300

    feat: resolvendo exercicio 3 - comissao

---
+++ Lista2/Ex3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Digite valor do salário fixo: ");
+            double salario = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite valor das vendas: ");
+            double totalVendas = double.Parse(Console.ReadLine());
+
+            double comissao = 0;
+
+            if(totalVendas > 0 && totalVendas <= 1500) {
+                comissao += totalVendas * 0.03;
+            } else if (totalVendas > 1500) {
+                comissao += (1500 * 0.03) + (totalVendas - 1500) * 0.05;
+            } else {
+                Console.WriteLine("Valor informado inválido!");
+            }
+
+            Console.WriteLine("Valor do salário: " + salario);
+            Console.WriteLine("Valor das vendas: " + totalVendas);
+            Console.WriteLine("Valor com comissão: " + (salario + comissao));
+
+            Console.ReadLine();
+        }
+    }
+}
