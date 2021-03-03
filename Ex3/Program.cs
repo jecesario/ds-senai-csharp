0f578be42a6fb2be73d1e65696dec2239be4220a
Jecesario <jp@soloid.com.br>
2021-03-03 11:52:09 -0300

    feat(ex3): resolvendo exercício 3 - raiz quadrada

---
+++ Ex3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("A raiz quadrada de " + n1 + " é igual a " + Math.Sqrt(n1));
+            Console.ReadLine();
+        }
+    }
+}
