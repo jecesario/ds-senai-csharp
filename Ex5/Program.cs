48357a4ebe5d8ac85e1a087db8c20b86c9ee3dc8
Jecesario <jp@soloid.com.br>
2021-03-03 12:00:28 -0300

    feat: resolvendo exercício 5 - exibindo ascii de caractere

---
+++ Ex5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            string a = Console.ReadLine();
+
+            byte[] ascii = Encoding.ASCII.GetBytes(a);
+
+            foreach (byte item in ascii)
+            {
+                Console.WriteLine("Valor ASCII: " + item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
