8936f2daba30d6a78871b7cde8daf6f102475347
Jecesario <jp@soloid.com.br>
2021-03-03 11:43:08 -0300

    feat(ex2): resolvendo exercício 2

---
+++ Ex2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            double n1 = double.Parse(Console.ReadLine());
+            Console.Write("Digite outro número: ");
+            double n2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Soma dos dois números: " + (n1 + n2));
+            Console.WriteLine("Subtração dos dois números: " + (n1 - n2));
+            Console.WriteLine("Multiplicação dos dois números: " + (n1 * n2));
+            Console.WriteLine("Divisão dos dois números: " + (n1 / n2));
+            Console.ReadLine();
+        }
+    }
+}
