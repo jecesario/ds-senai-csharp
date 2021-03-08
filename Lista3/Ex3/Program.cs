1afdc369acf9f98114d2f520e1924612c6cb5598
Jecesario <jp@soloid.com.br>
2021-03-08 20:00:26 -0300

    feat: resolvendo exercício 3 - divisao de numeros

---
+++ Lista3/Ex3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3 {
+    class Program {
+        static void Main(string[] args) {
+
+            double a;
+            double b;
+
+            Console.Write("Informe um numero positivo: ");
+            a = double.Parse(Console.ReadLine());
+
+            while(a <= 0) {
+                Console.Write("Informe um numero positivo: ");
+                a = double.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Informe outro numero positivo: ");
+            b = double.Parse(Console.ReadLine());
+
+            while (b <= 0) {
+                Console.Write("Informe outro numero positivo: ");
+                b = double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("O numero " + a + " dividido por " + b + " é igual a " + (a / b));
+
+            Console.ReadLine();
+
+        }
+    }
+}
