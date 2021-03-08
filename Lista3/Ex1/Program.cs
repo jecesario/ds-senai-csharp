5a337bc27336c5409e55261e934c1301aaa5bc08
Jecesario <jp@soloid.com.br>
2021-03-08 19:57:31 -0300

    feat: resolvendo exercício 1 - imprimir numeros em escada

---
+++ Lista3/Ex1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1 {
+    class Program {
+        static void Main(string[] args) {
+
+            String texto = "";
+
+            for(int i=1; i<8; i++) {
+                texto += i;
+                Console.WriteLine(texto);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
