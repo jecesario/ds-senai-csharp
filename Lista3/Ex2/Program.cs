0ec770c1d0b3ef0c302d90d4b9a9eefa33f2f6eb
Jecesario <jp@soloid.com.br>
2021-03-08 19:58:10 -0300

    feat: resolvendo exercício 2 - imprimir numeros em escada com asterisco

---
+++ Lista3/Ex2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2 {
+    class Program {
+        static void Main(string[] args) {
+
+            for (int i = 1; i <= 7; i++) {
+                for (int x = 1; x <= 7; x++) {
+                    if (x <= i) {
+                        Console.Write(x);
+                    } else {
+                        Console.Write("*");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
